81f6de0d6db9595b9ce569f9c1a79f63b7e1b81b
ingridpm <69186270+ingridpm@users.noreply.github.com>
2021-02-27 17:39:11 -0600

    Update daily.sh

--- daily.sh
+++ daily.sh
@@ -2,6 +2,5 @@
 #Created 17 June 2014
 #Author : Jignesh Jethwa
 #Version 1.0
-#Set Environment Variable
-mkdir hola
-#rman target / @/home/scripts/bd2_grupo7/script.rcv
+#Set Environment Variabl
+rman target / @/home/scripts/bd2_grupo7/script.rcv
