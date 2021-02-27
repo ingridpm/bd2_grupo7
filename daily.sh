a8137a6a600f5f3bea684f65ac0c85ac43581f4b
ingridpm <69186270+ingridpm@users.noreply.github.com>
2021-02-27 17:51:56 -0600

    Update daily.sh

--- daily.sh
+++ daily.sh
@@ -7,4 +7,4 @@ export ORACLE_SID=ORCL18
 export ORACLE_BASE=/u01/app/oracle
 export ORACLE_HOME=/u01/app/oracle/product/18.0.0/dbhome_1
 export PATH=$ORACLE_HOME/bin:$PATH
-rman target / @/home/scripts/bd2_grupo7/script.rcv
+/u01/app/oracle/product/18.0.0/dbhome_1/bin/rman target / @/home/scripts/bd2_grupo7/script.rcv
