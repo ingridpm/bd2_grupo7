d505f1dd39b2a00f8ddb8009c8b35114d3a64307
ingridpm <69186270+ingridpm@users.noreply.github.com>
2021-02-27 18:06:57 -0600

    Update daily.sh

--- daily.sh
+++ daily.sh
@@ -3,10 +3,9 @@
 #Author : Jignesh Jethwa
 #Version 1.0
 #Set Environment Variabl
-export ORACLE_BASE=/u01/app/oracle
-export ORACLE_SID=ORCL18
-export ORACLE_HOME=/u01/app/oracle/product/18.0.0/dbhome_1
-export PATH=$ORACLE_HOME/bin:$PATH
+#export ORACLE_BASE=/u01/app/oracle
+#export ORACLE_SID=ORCL18
+#export ORACLE_HOME=/u01/app/oracle/product/18.0.0/dbhome_1
+#export PATH=$ORACLE_HOME/bin:$PATH
 #export PATH=$PATH:$ORACLE_HOME/bin
 /u01/app/oracle/product/18.0.0/dbhome_1/bin/rman target / @/home/scripts/bd2_grupo7/script.rcv
-exit
